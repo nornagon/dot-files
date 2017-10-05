cc04defc98ab76b6e9433346250268b13d3cb045
Jeremy Apthorp <nornagon@nornagon.net>
2017-10-05 16:28:41 -0700

    Update zsh keybinds

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -1,16 +1,17 @@
 bindkey -e
  
 # C-left and C-right to go back/forward a word.
+# NB. in OS X, these are bound to Mission Control by default, and you'll need
+# to disable them there before these will work.
 bindkey '\e[1;5D' backward-word
 bindkey '\e[1;5C' forward-word
+
 # for URxvt
 bindkey '\eOd' backward-word
 bindkey '\eOc' forward-word
-# for Terminal.app
-bindkey '\e[5D' backward-word
-bindkey '\e[5C' forward-word
+
+# for iTerm2
+bindkey '\e[3~' delete-char
+
 bindkey '\e[H' beginning-of-line
 bindkey '\e[F' end-of-line
-bindkey "[1~" beginning-of-line
-bindkey "[4~" end-of-line
-bindkey "[3~" delete-char
