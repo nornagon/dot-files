e4c3924c80a712a6954a96932cd7ec4e1798be0b
Jeremy Rose <nornagon@nornagon.net>
2024-09-30 17:06:50 -0700

    ps1_dir_color

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,6 +2,18 @@ function __ps1_git_state() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo ' %{'$fg[red]'%}'${ref#refs/heads/}'%{'$terminfo[sgr0]'%}'
 }
+
+function __ps1_dir_color() {
+  # Hash PWD into a custom background color for iterm
+  # form: ESC]11;rgb:RRRR/GGGG/BBBB/ST
+  # e.g. echo -e "\033]11;rgb:1a1a/1a1a/1a1a\007"
+  local h
+  h=$(echo -n $PWD | md5sum)
+  local r=$(printf "%02x" $(( (0x${h:1:2} / 16) )))
+  local g=$(printf "%02x" $(( (0x${h:3:2} / 16) )))
+  local b=$(printf "%02x" $(( (0x${h:5:2} / 16) )))
+  echo -ne "%{\e]11;rgb:${r}/${g}/${b}\007%}"
+}
  
 function __custom_pwd() {
   local d slash
@@ -36,7 +48,7 @@ if [[ -n $MYVIMRC ]]; then
   _bracket_color='%{$fg_bold[green]%}'
 fi
  
-PS1=$_bracket_color'[\
+PS1=$_bracket_color'[$(__ps1_dir_color)\
 %{'$_hostcolor'%}%m%{$fg_bold[red]%}:%{$terminfo[sgr0]%}$(__custom_pwd)\
 $(__ps1_git_state)\
 '$_bracket_color']%{$terminfo[sgr0]%} '
